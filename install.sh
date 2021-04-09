4aab90651eadc4a7167e995ec04beebdef870519
matheusfillipe <matheusfillipeag@gmail.com>
2021-04-09 19:03:57 -0300

    coc-jedi

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ then
   cp ~/.config/nvim/init.vim nvim/
   cp ~/.config/nvim/coc-settings.json nvim/
   cp ~/.config/vifm/vifmrc vifm/
-  cp ~/.novimZsh/
+  cp ~/.novimZsh/ .
   cp ~/.novimZsh/.zshrc .novimZsh/
   git add .; git commit -m "$2"; git push origin master;
   exit 0
